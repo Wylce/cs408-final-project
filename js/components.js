ee2a0ebdf1329a1fdab36cfd40dc8e35c61f226d
Wylce <122563162+Wylce@users.noreply.github.com>
2024-11-13 23:27:24 -0700

    Tweaked gallery link in header to go to page 1 specifically

--- js/components.js
+++ js/components.js
@@ -4,7 +4,7 @@ document.querySelector(".write-header").innerHTML = `
     <nav>
       <ul>
         <li><a href="../index.html">About</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="gallery.html?page=1">Gallery</a></li>
         <li><a href="comic-index.html">Index</a></li>
         <li><a href="artist.html">Artist</a></li>
       </ul>
